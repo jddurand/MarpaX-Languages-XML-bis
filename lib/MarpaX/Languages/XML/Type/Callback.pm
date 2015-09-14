1b244e3831a825516074868f2afd86abf7eb4bb6
Jean-Damien Durand <jeandamiendurand@free.fr>
2015-09-14 06:40:51 +0200

    dev+: Type/Callback.pm

---
+++ lib/MarpaX/Languages/XML/Type/Callback.pm
@@ -0,0 +1,15 @@
+package MarpaX::Languages::XML::Type::Callback;
+use Type::Library
+  -base,
+  -declare => qw/Callback/;
+use Type::Utils -all;
+use Types::Standard -all;
+use Types::Common::Numeric -all;
+
+declare Callback,
+  as Dict[
+          name => Str,
+          code => CodeRef
+         ];
+
+1;
